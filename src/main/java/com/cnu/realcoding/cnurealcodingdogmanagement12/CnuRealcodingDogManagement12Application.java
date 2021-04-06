340147ef8fb88dbf61ed41889581d73e27f5ba20
Euijae Lee <tenitlee@gmail.com>
2021-04-07 00:30:37 +0900

    First Commit

---
+++ src/main/java/com/cnu/realcoding/cnurealcodingdogmanagement12/CnuRealcodingDogManagement12Application.java
@@ -0,0 +1,13 @@
+package com.cnu.realcoding.cnurealcodingdogmanagement12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnuRealcodingDogManagement12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnuRealcodingDogManagement12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cnu/realcoding/cnurealcodingdogmanagement12/CnuRealcodingDogManagement12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnu.realcoding.cnurealcodingdogmanagement12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnuRealcodingDogManagement12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
